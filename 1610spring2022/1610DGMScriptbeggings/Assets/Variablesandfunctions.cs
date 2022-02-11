7bfb2aec3c2e7fb32010cb55885e1d68a8c1d72d
Kami Gossard <drassogimak@gmail.com>
2022-02-10 17:46:51 -0700

    variable and functions

---
+++ 1610spring2022/1610DGMScriptbeggings/Assets/Variablesandfunctions.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VariablesAndFunctions : MonoBehaviour
+{
+    int myInt = 5 ;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myInt = MultiplyByTwo(5)
+            Debug.log (5)
+
+    }
+int MutiplyByTwo (5)
+    // Update is called once per frame
+    void Update()
+    {
+    int  result;
+    result = number * 2; 
+    return result; 
+    }
+
