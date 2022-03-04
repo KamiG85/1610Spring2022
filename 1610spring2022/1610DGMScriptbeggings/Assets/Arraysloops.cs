551c88620e7928ef7388d9e571e394a387c79c4c
Kami Gossard <drassogimak@gmail.com>
2022-03-03 18:43:09 -0700

    Arrays with while loops,forloops andforeach loop

---
+++ 1610spring2022/1610DGMScriptbeggings/Assets/Arraysloops.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arraysloops : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    private string[] cars;
+    {
+        string[] cars = {"Volvo", "BMW", "Ford", "Mazda", "GMC", "Toyota"};
+        Console.WriteLine(cars[0]);
+        string[] cars = {"Volvo", "BMW", "Ford", "Mazda", "GMC", "Toyota"};
+        Console.WriteLine(cars.Length);
+        string[] cars = {"Volvo", "BMW", "Ford", "Mazda", "GMC", "Toyota"};
+        for (int i = 0; i < cars.Length; i++) 
+            Console.WriteLine(cars[i]);
+        string[] cars = {"Volvo", "BMW", "Ford", "Mazda", "GMC", "Toyota"};
+        foreach (string i in cars)
+            Console.WriteLine(i);
+        string[] cars = {"Volvo", "BMW", "Ford", "Mazda", "GMC", "Toyota"};
+        do
+        {
+            Console.WriteLine(i);
+        } while (i < 5);
+
+    }
+
+
+    }
+    
+
+    }
